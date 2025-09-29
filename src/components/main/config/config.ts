6040bb689bb5abf1f65359387fc03c3ca8907b3f
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2025-09-29 01:32:06 -0500

    repositories as help

--- src/components/main/config/config.ts
+++ src/components/main/config/config.ts
@@ -1,14 +1,14 @@
 import { Cartesian3, Math as CesiumMath } from "cesium";
 
 export const targetLocation = {
-  // destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
   destination: Cartesian3.fromDegrees(
-    9.041914842288406,
-    48.833119059728752,
-    20
+    -71.967,   // longitud
+    -13.517,   // latitud
+    200        // altura de la cámara en metros
   ),
   orientation: {
-    heading: CesiumMath.toRadians(0.0),
-    pitch: CesiumMath.toRadians(-15.0),
+    heading: CesiumMath.toRadians(0.0),   // 0 = norte, puedes girar (90 = este, 180 = sur)
+    pitch: CesiumMath.toRadians(-30.0),   // -30° inclina la cámara hacia abajo
+    roll: 0.0,
   },
 };
